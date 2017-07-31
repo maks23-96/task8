6e058a96f7c815f8aafad0032c5b1b88524112a0
Student 5 <diskakun@gmail.com>
2017-07-31 18:21:18 +0300

    commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include('config.php');
+include('libs/Curl.php');
+
+
+
+include('templates/index.php');
+?>
---
+++ templates/index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+  <title>Search</title>
+  <link href="css/bootstrap.css" rel="stylesheet"></link>
+</head>
+<body>
+
+</body>
+</html>
